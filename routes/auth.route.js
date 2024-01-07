f596eab6d376e0ea103a38d2d79061470aae9b6a
Bryan Mancilla <70993543@certus.edu.pe>
2024-01-07 18:00:47 -0500

    Ruta login

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -4,5 +4,6 @@ const { createUser } = require('../controller/user.controller');
 const router = express.Router();
 
 router.post('/register', createUser)
+router.post('/login', createUser)
 
 module.exports = router;
