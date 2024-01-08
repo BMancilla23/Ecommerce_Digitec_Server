c01fa3f67e639cfb92cbabda15e954ef65f516b1
Bryan Mancilla <70993543@certus.edu.pe>
2024-01-07 23:38:35 -0500

    Se estableció las rutas del usuario

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,9 +1,14 @@
 const express = require('express');
-const { createUser, loginUserCtrl } = require('../controller/user.controller');
+const { createUser, loginUserCtrl, getAllUser, getAUser, deleteAUser, updatedUser } = require('../controller/user.controller');
 
 const router = express.Router();
 
 router.post('/register', createUser)
 router.post('/login', loginUserCtrl)
+router.get('/all-users', getAllUser)
+router.get('/:id', getAUser)
+router.delete('/:id', deleteAUser)
+router.put('/:id', updatedUser)
 
 module.exports = router;
+
