380a3ca58f77335940c58a842a6ef90053418f96
Bryan Mancilla <70993543@certus.edu.pe>
2024-01-08 17:03:13 -0500

    Inregración de handleRefreshToken para renovación de tokens

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,6 +1,9 @@
 const { generateToken } = require("../config/jwtToken");
 const User = require("../models/user.schema");
 const asyncHandler = require("express-async-handler");
+const validateMongoDbId = require("../utils/validateMongodbId");
+const { generateRefreshToken } = require("../config/refreshToken");
+const jwt = require("jsonwebtoken")
 
 const createUser = asyncHandler(async (req, res) => {
   const email = req.body.email;
@@ -33,7 +36,20 @@ const loginUserCtrl = asyncHandler(async (req, res) => {
     const isPasswordMatched = await findUser.isPasswordMatched(password);
 
     if (isPasswordMatched) {
-      // Contraseña válida, puedes generar un token de autenticación aquí si es necesario
+      
+      // Contraseña váñida, genera un token de autenticación
+      const refreshToken = await generateRefreshToken(findUser?._id)
+      // Actualiza el usuario con el nuevo token de actualización
+      const updateUser = await User.findByIdAndUpdate(findUser.id, {
+        refreshToken: refreshToken
+      }, {new: true})
+
+      // Establece la cookie con el token de actualización
+      res.cookie('refreshToken', refreshToken, {
+        httpOnly: true,
+        maxAge: 72 * 60 * 60 * 1000
+      })
+
       res.json({
         _id: findUser?._id,
         firstname: findUser?.firstname,
@@ -58,12 +74,48 @@ const loginUserCtrl = asyncHandler(async (req, res) => {
   }
 });
 
+// Handle refresh token
+const handleRefreshToken = asyncHandler(async (req, res) => {
+  // Obtiene las cookies de la solicitud
+  const cookie = req.cookies;
+
+  // Verifica si existe un refreshToken en las cookies
+  if (!cookie.refreshToken) {
+    throw new Error('No refresh Token in Cookies');
+  }
+
+  // Obtiene el refreshToken de las cookies
+  const refreshToken = cookie.refreshToken;
+
+  // Busca al usuario correspondiente al refreshToken en la base de datos
+  const user = await User.findOne({ refreshToken });
+
+  // Verifica si se encontró un usuario
+  if (!user) {
+    throw new Error('No Refresh token present in db or not matched');
+  }
+
+  // Verifica la validez del refreshToken utilizando la clave secreta del JWT
+  jwt.verify(refreshToken, process.env.JWT_SECRET, (err, decoded) => {
+    // Verifica si hay un error o si el id del usuario no coincide con el id decodificado
+    if (err || user.id !== decoded.id) {
+      throw new Error('There is something wrong with refresh token');
+    }
+
+    // Si la verificación es exitosa, genera un nuevo accessToken y lo envía en la respuesta
+    const accessToken = generateToken(user?._id);
+    res.json({ accessToken });
+  });
+});
+
 // Update a user
 const updatedUser = asyncHandler(async (req, res) => {
-  const { id } = req.params;
+  console.log(req.user);
+  const { _id } = req.user;
+  validateMongoDbId(_id);
 
   const updatedUser = await User.findByIdAndUpdate(
-    id,
+    _id,
     {
       firstname: req?.body?.firstname,
       lastname: req?.body?.lastname,
@@ -87,6 +139,7 @@ const getAllUser = asyncHandler(async (req, res) => {
 // Get a single user
 const getAUser = asyncHandler(async (req, res) => {
   const { id } = req.params;
+  validateMongoDbId(id)
 
   const getAUser = await User.findById(id);
   res.json({
@@ -97,6 +150,7 @@ const getAUser = asyncHandler(async (req, res) => {
 // Delete a single user
 const deleteAUser = asyncHandler(async (req, res) => {
   const { id } = req.params;
+  validateMongoDbId(id);
 
   const deleteAUser = await User.findOneAndDelete(id);
   res.json({
@@ -104,11 +158,48 @@ const deleteAUser = asyncHandler(async (req, res) => {
   });
 });
 
+const blockUser = asyncHandler(async(req, res) => {
+  const {id} = req.params;
+  validateMongoDbId(id);
+  const block = await User.findByIdAndUpdate(
+    id,
+    {
+      isBlocked: true,
+    },
+    {
+      new: true
+    }
+  )
+  res.json({
+    message: "User Blocked"
+  })
+})
+
+const unBlockUser = asyncHandler(async(req, res) => {
+  const {id} = req.params;
+  validateMongoDbId(id);
+  const unblock = await User.findByIdAndUpdate(
+    id,
+    {
+      isBlocked: false,
+    },
+    {
+      new: true
+    }
+  )
+  res.json({
+    message: "User Unblocked"
+  })
+})
+
 module.exports = {
   createUser,
   loginUserCtrl,
   getAllUser,
   getAUser,
   deleteAUser,
-  updatedUser
+  updatedUser,
+  blockUser,
+  unBlockUser,
+  handleRefreshToken
 };
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { createUser, loginUserCtrl, getAllUser, getAUser, deleteAUser, updatedUser, blockUser, unBlockUser } = require('../controller/user.controller');
+const { createUser, loginUserCtrl, getAllUser, getAUser, deleteAUser, updatedUser, blockUser, unBlockUser, handleRefreshToken } = require('../controller/user.controller');
 const {authMiddleware, isAdmin} = require('../middlewares/authMiddleware');
 
 const router = express.Router();
@@ -7,11 +7,14 @@ const router = express.Router();
 router.post('/register', createUser)
 router.post('/login', loginUserCtrl)
 router.get('/all-users', getAllUser)
+router.get('/refresh', handleRefreshToken)
+
 router.get('/:id', authMiddleware, isAdmin, getAUser)
 router.delete('/:id', deleteAUser)
 router.put('/edit-user',authMiddleware, updatedUser)
 router.put('/block-user/:id', authMiddleware, isAdmin, blockUser)
 router.put('/unblock-user/:id', authMiddleware, isAdmin, unBlockUser)
+
 
 module.exports = router;
 
