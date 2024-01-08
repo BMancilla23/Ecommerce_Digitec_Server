97a62724730ef2d28c1d51e43b70488c8b2b4581
Bryan Mancilla <70993543@certus.edu.pe>
2024-01-08 13:42:07 -0500

    Uso de middleware cookie parser

--- index.js
+++ index.js
@@ -6,11 +6,14 @@ const dotenv = require('dotenv').config()
 const PORT = process.env.PORT || 4000
 const authRouter = require('./routes/auth.route')
 const { notFound, errorHandler } = require('./middlewares/errorHandler')
+const cookieParser = require('cookie-parser')
 
 dbConnect()
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+app.use(cookieParser())
+
 app.use('/api/user', authRouter)
 
 app.use(notFound)
