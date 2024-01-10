69c99e62a1c2dce88445095546402fb520b54376
Bryan Mancilla <70993543@certus.edu.pe>
2024-01-10 17:43:46 -0500

    Se agregó la ruta productos y se habilitó morgan

--- index.js
+++ index.js
@@ -5,16 +5,20 @@ const app = express()
 const dotenv = require('dotenv').config()
 const PORT = process.env.PORT || 4000
 const authRouter = require('./routes/auth.route')
+const productRouter = require('./routes/product.route')
 const { notFound, errorHandler } = require('./middlewares/errorHandler')
 const cookieParser = require('cookie-parser')
+const morgan = require('morgan')
 
 dbConnect()
 
+app.use(morgan("dev"))
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use(cookieParser())
 
 app.use('/api/user', authRouter)
+app.use('/api/product', productRouter)
 
 app.use(notFound)
 app.use(errorHandler)
