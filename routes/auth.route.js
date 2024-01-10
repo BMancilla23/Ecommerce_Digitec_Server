d2ce59ae726f0bfa299d8375101857709a293aa4
Bryan Mancilla <70993543@certus.edu.pe>
2024-01-10 17:46:35 -0500

    Ruta para cambiar contraseña

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,17 +1,18 @@
 const express = require('express');
-const { createUser, loginUserCtrl, getAllUser, getAUser, deleteAUser, updatedUser, blockUser, unBlockUser, handleRefreshToken, logout } = require('../controller/user.controller');
+const { createUser, loginUserCtrl, getAllUser, getAUser, deleteAUser, updatedUser, blockUser, unBlockUser, handleRefreshToken, logout, updatePassword } = require('../controller/user.controller');
 const {authMiddleware, isAdmin} = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
 router.post('/register', createUser)
+router.put('/password', authMiddleware, updatePassword)
 router.post('/login', loginUserCtrl)
 router.get('/all-users', getAllUser)
 router.get('/refresh', handleRefreshToken)
 router.get('/logout', logout)
-
 router.get('/:id', authMiddleware, isAdmin, getAUser)
 router.delete('/:id', deleteAUser)
+
 router.put('/edit-user',authMiddleware, updatedUser)
 router.put('/block-user/:id', authMiddleware, isAdmin, blockUser)
 router.put('/unblock-user/:id', authMiddleware, isAdmin, unBlockUser)
